a15bf74292a23f554ca2af97f16a1624938a3c2a
Solkind <119994828+Solkind@users.noreply.github.com>
2023-02-15 22:58:20 -0500

    initial commit
    Here's an implementation of a weighted lottery function in JavaScript that uses the weights object provided and the bitwise operators to generate a random result based on the weighted probabilities

---
+++ src/bootstrap.js
@@ -0,0 +1,37 @@
+// My Solution
+const weights = { green: 1, yellow: 2, red: 3 }
+
+function weightedLottery(weights) {
+  const totalWeight = Object.values(weights).reduce((acc, weight) => acc + weight, 0);
+  let randomNum = (Math.random() * totalWeight) | 0;
+
+
+  for (let key of Object.keys(weights)) {
+    const weight = weights[key];
+    if (randomNum < weight) {
+      return key;
+    }
+    randomNum -= weight;
+  }
+}
+console.log(weightedLottery(weights))
+
+// BottegaU Solution
+// const weightedLottery = weights => {
+//   let containerArray = [];
+
+//   Object.keys(weights).forEach(key => {
+//     for (let i = 0; i < weights[key]; i++) {
+//       containerArray.push(key);
+//     }
+//   });
+
+//   return containerArray[(Math.random() * containerArray.length) | 0];
+// };
+
+// const weights = {
+//   green: 1,
+//   yellow: 2,
+//   red: 3
+// };
+// console.log(weightedLottery(weights));
